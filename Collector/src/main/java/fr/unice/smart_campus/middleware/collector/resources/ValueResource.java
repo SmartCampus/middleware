7433c35f47bc2e0b09d932ac90243cc407549e90
Thomas Di'Meco <thomas.dimeco@gmail.com>
2014-01-22 18:58:21 +0100

    Collector: POST values that display posted values

--- Collector/src/main/java/fr/unice/smart_campus/middleware/collector/resources/ValueResource.java
+++ Collector/src/main/java/fr/unice/smart_campus/middleware/collector/resources/ValueResource.java
@@ -1,16 +1,77 @@
 package fr.unice.smart_campus.middleware.collector.resources;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.codehaus.jettison.json.JSONTokener;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import java.util.Date;
+
+import static javax.ws.rs.core.Response.Status;
 
 
 @Path("value")
 public class ValueResource {
 
+	@POST
+	@Produces("text/plain")
+	@Consumes("application/json")
+	public Response postValue (String jsonString) {
+
+		String errorMessage = null;
+
+		try {
+			JSONObject json = new JSONObject(new JSONTokener(jsonString));
+
+			String name = json.getString("n");
+			String value = json.getString("v");
+			String time = json.getString("t");
+
+			Date date = new Date(Long.parseLong(time) * 1000);
+
+			// TODO: TEMP: Display values
+			System.out.println(
+				"Received: name = " + name + ", value = " + value + ", time = " + time + " (" + date + ");");
+
+		} catch (JSONException exc) {
+			errorMessage = exc.getMessage();
+		} catch (NumberFormatException exc) {
+			errorMessage = "Wrong timestamp format";
+		}
+
+		if (errorMessage != null) {
+			return Response
+					.status(Status.BAD_REQUEST)
+					.entity("Error: " + errorMessage)
+					.build();
+		}
+
+		return Response
+				.status(Status.CREATED)
+				.entity("Value posted with success!")
+				.build();
+	}
+
+
 	@GET
 	@Produces("text/plain")
-	public String getMessage () {
-		return "Hello World!";
+	public Response getValue () {
+		return getWrongCommandErrorResponse();
+	}
+
+
+	@PUT
+	@Produces("text/plain")
+	public Response putValue () {
+		return getWrongCommandErrorResponse();
+	}
+
+
+	private static Response getWrongCommandErrorResponse () {
+		return Response
+				.status(Status.NOT_FOUND)
+				.entity("Error: Use the POST HTTP command to post a value into the collector")
+				.build();
 	}
 }

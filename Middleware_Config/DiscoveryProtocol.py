4c4cc6d85cccf2d51205f8f781878da835a725e7
matthieu jimenez <electricalwind@gmail.com>
2014-01-27 16:24:36 +0100

    Commentaires améliorés

--- Middleware_Config/DiscoveryProtocol.py
+++ Middleware_Config/DiscoveryProtocol.py
@@ -3,15 +3,25 @@ import http.client
 import json
 
 class DiscoveryProtocol:
+    '''
+    class that implement the discovery Protocol
+
+    Parameters:
+    connectingToBridge -- it's an HTTPConnection object containing the adress of the bridge to reach
+    '''
     def __init__(self,ipAdress,port):
+        '''
+        Constructor of the Discovery Protocol class
+
+        Keywords Arguments:
+        ipAdress -- IP adress if the bridge to reach
+        port -- Port number of the REST interface of the bridge
+        '''
         self.connectingToBridge= http.client.HTTPConnection(ipAdress,port)
 
     def discoverRequest(self):
         '''
         Method to retrieve the board connected to a bridge at a given URL
-
-        Keyword argument:
-        connectingToBridge -- it's an HTTPConnection object containing the adress of the bridge to reach
 
         Return:
         BoardsJson -- Json Object conataing the list of all Boards connected to the Bridge
@@ -31,7 +41,9 @@ class DiscoveryProtocol:
 
     def putConfig(self,confInJson):
         '''
+        Methode to push Json conf of the sensors to the bridge
 
+        Return the result of the PUT request
         '''
         try:
             self.connectingToBridge.request("PUT","/config",confInJson)

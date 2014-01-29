0d09f05b85638578009caaa2414fe5e17120af92
matthieu jimenez <electricalwind@gmail.com>
2014-01-29 21:22:31 +0100

    Modification pour rester en accord avec recommandations PEP8

--- sql_database/insert.sql
+++ sql_database/insert.sql
@@ -10,10 +10,13 @@ INSERT INTO bridge(
     VALUES ('pi-1','home.cecchinel.fr','9001','pi 1','appartement de cyril');
 INSERT INTO capteur(
             idcapteur, nom, description, frequency, type)
-    VALUES ('ctempcyr','capt-temp-cyril','capteur de temperature chez cyril',5,'temp');
+    VALUES ('ctempcyr','capt-temp-cyril','capteur de temperature chez cyril',5,'temp'),
+           ('ctempmat','capt-temp-mat','capteur de temperature chez matthieu',10,'temp');
 INSERT INTO capteurphy(
             idcapteur, timestart, timestop, boardpin, endpointIP, port, idboard)
-    VALUES ('ctempcyr', '6:0', '6:0', '8', 'home.dimecco.fr',5001, 'ard-101');
+    VALUES ('ctempcyr', '6:0', '16:0', '8', 'home.dimecco.fr',5001, 'ard-101'),
+           ('ctempmat', '6:0', '16:0', '8', 'home.dimecco.fr',5001, 'ard-101')
+;
 --INSERT INTO capteurvirt(
   --          idcapteur, script)
     --VALUES (?, ?);

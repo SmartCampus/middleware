3eafc5b39586e156e046d3932e6e665fe3244688
matthieu jimenez <electricalwind@gmail.com>
2014-01-29 18:26:21 +0100

    Creation de la base de données + insert de test

---
+++ sql_database/create.sql
@@ -0,0 +1,66 @@
+drop table CorrespondT;
+drop table Tag;
+drop table CapteurPhy;
+drop table CapteurVirt;
+drop table Capteur;
+drop table Bridge;
+drop table TypeCapteur;
+drop table Board;
+
+create table Bridge(
+		idBridge VARCHAR(16),
+		ip VARCHAR(32),
+		port int4,
+		nom VARCHAR(32),
+		loc VARCHAR(32),
+		PRIMARY KEY (idBridge)
+);
+CREATE TABLE Board (
+		idBoard VARCHAR(16),
+		nom VARCHAR(16),
+		description VARCHAR (32),
+		PRIMARY KEY (idBoard)
+);
+
+CREATE TABLE TypeCapteur (
+  idtype VARCHAR(16),
+  description VARCHAR(16),
+  PRIMARY KEY (idtype)
+);
+
+CREATE TABLE Capteur (
+		idCapteur VARCHAR(16),
+		nom VARCHAR(32),
+    description text,
+    frequency numeric,
+		type VARCHAR(16) NOT NULL REFERENCES TypeCapteur,
+		PRIMARY KEY (idCapteur)
+);
+
+CREATE TABLE CapteurPhy (
+		idCapteur VARCHAR(16) NOT NULL REFERENCES Capteur,
+		timeStart time,
+    timeStop time,
+    boardPin VARCHAR (8),
+    endPointIP VARCHAR (32),
+    port int4,
+    idBoard VARCHAR (16) REFERENCES Board,
+		PRIMARY KEY (idCapteur)
+);
+CREATE TABLE CapteurVirt (
+    idCapteur VARCHAR(16) NOT NULL REFERENCES Capteur,
+    script TEXT,
+    PRIMARY KEY (idCapteur)
+);
+
+CREATE TABLE Tag (
+    name VARCHAR (16),
+    description text,
+    PRIMARY KEY (name)
+);
+CREATE TABLE CorrespondT (
+		idCapteur VARCHAR(16) NOT NULL REFERENCES Capteur,
+		name VARCHAR (16) NOT NULL REFERENCES Tag,
+		PRIMARY KEY (idCapteur,name)
+);
+
---
+++ sql_database/insert.sql
@@ -0,0 +1,26 @@
+insert into Tag VALUES ('untag', 'description dun tag');
+INSERT INTO typecapteur(
+            idtype, description)
+    VALUES ('temp','temperature');
+INSERT INTO board(
+            idboard, nom, description)
+    VALUES ('ard-101','mon arduino','arduino number 1');
+INSERT INTO bridge(
+            idbridge, ip, port, nom, loc)
+    VALUES ('pi-1','home.cecchinel.fr','9001','pi 1','appartement de cyril');
+INSERT INTO capteur(
+            idcapteur, nom, description, frequency, type)
+    VALUES ('ctempcyr','capt-temp-cyril','capteur de temperature chez cyril',5,'temp');
+INSERT INTO capteurphy(
+            idcapteur, timestart, timestop, boardpin, endpointIP, port, idboard)
+    VALUES ('ctempcyr', '6:0', '6:0', '8', 'home.dimecco.fr',5001, 'ard-101');
+--INSERT INTO capteurvirt(
+  --          idcapteur, script)
+    --VALUES (?, ?);
+--INSERT INTO correspondt(
+  --          idcapteur, name)
+    --VALUES (?, ?);
+--INSERT INTO tag(
+  --          name, description)
+    --VALUES (?, ?);
+

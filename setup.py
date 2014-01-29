6955a89e47b2bb86cb2a330ea651e58c601aaaa5
matthieu jimenez <electricalwind@gmail.com>
2014-01-29 21:25:01 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name="Middleware_Config",
     packages=["Middleware_Config"],
-    version="0.0.1",
+    version="0.0.2",
     author="org.unice.fr.smartcampus",
     author_email="",
     url="localhost"

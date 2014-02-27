aec1647102369c2000cc4b016bca95fc5f2ab973
Thomas Di'Meco <thomas.dimeco@gmail.com>
2014-02-27 12:20:01 +0100

    Processor: Database insertion

--- data-processor/src/main/java/fr/unice/smart_campus/middleware/processor/App.java
+++ data-processor/src/main/java/fr/unice/smart_campus/middleware/processor/App.java
@@ -1,5 +1,7 @@
 package fr.unice.smart_campus.middleware.processor;
 
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import javax.jms.JMSException;
 import javax.jms.Message;
@@ -13,19 +15,51 @@ import javax.jms.TextMessage;
  */
 public class App implements MessageListener {
 
-	public static void main (String[] args) throws JMSException {
-		DataAccess.createMessageListener(new App());
+	private OutputDataAccess outputDataAccess;
+
+
+	public App () throws Exception {
+
+		// Create output proxy
+		outputDataAccess = new OutputDataAccess();
+
+		// Create input listener
+		InputDataAccess.createMessageListener(this);
 	}
 
+
+	/**
+	 * Executed when a message is received from the message queue
+	 * @param message The sensor data message
+	 */
 	@Override
 	public void onMessage (Message message) {
 		if (message instanceof TextMessage) {
-			TextMessage jsonMessage = (TextMessage) message;
 			try {
-				System.out.println(jsonMessage.getText());
+				String jsonString = ((TextMessage) message).getText();
+
+				// TODO: TEMP: Print received JSON message
+				System.out.println(jsonString);
+
+				// Extract sensor information
+				JSONObject jsonObject = new JSONObject(new JSONTokener(jsonString));
+				String name  = jsonObject.getString("n");
+				String time  = jsonObject.getString("t");
+				String value = jsonObject.getString("v");
+
+				// TODO: Make the value pass through multiples processors to process it
+
+				// Save sensor data into the database
+				outputDataAccess.saveSensorData(name, time, value);
+
 			} catch (JMSException e) {
 				e.printStackTrace();
 			}
 		}
+	}
+
+
+	public static void main (String[] args) throws Exception {
+		new App();
 	}
 }

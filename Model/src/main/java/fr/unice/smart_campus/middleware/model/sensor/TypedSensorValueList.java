cc7ecc71572520cd402d9fa4c6ef3752b2af65be
Jerome Rancati <jerome.rancati@gmail.com>
2015-02-27 08:02:22 +0100

    refactor & Co

--- Model/src/main/java/fr/unice/smart_campus/middleware/model/sensor/TypedSensorValueList.java
+++ Model/src/main/java/fr/unice/smart_campus/middleware/model/sensor/TypedSensorValueList.java
@@ -6,11 +6,41 @@ import java.util.List;
 
 public class TypedSensorValueList implements Serializable {
     private static final long serialVersionUID = 7526479295622776148L;
-
+    private String script;
+    private String virtualSensorName;
     private List<TypedSensorValue> sensorValues;
 
+    /**
+     * Constructor of TypedSensorValueList
+     * Construct an empty object
+     */
     public TypedSensorValueList() {
         this.sensorValues = new ArrayList<TypedSensorValue>();
+    }
+
+    /**
+     * Constructor of TypedSensorValueList
+     * Construct the object from the name and the script
+     * @param script the script to create the value of the virtual sensor named virtualSensorName
+     * @param virtualSensorName the name of the virtual sensor that have to be compute
+     */
+    public TypedSensorValueList(String script, String virtualSensorName) {
+        this.script = script;
+        this.virtualSensorName = virtualSensorName;
+        this.sensorValues = new ArrayList<TypedSensorValue>();
+    }
+
+    /**
+     * Constructor of TypedSensorValueList
+     * Construct the object from the name and the script
+     * @param script the script to create the value of the virtual sensor named virtualSensorName
+     * @param virtualSensorName the name of the virtual sensor that have to be compute
+     * @param sensorValues the list of the parents of the virtual sensor virtualSensorName.
+     */
+    public TypedSensorValueList(String script, String virtualSensorName, List<TypedSensorValue> sensorValues) {
+        this.script = script;
+        this.virtualSensorName = virtualSensorName;
+        this.sensorValues = sensorValues;
     }
 
     public List<TypedSensorValue> getSensorValues() {
@@ -25,14 +55,28 @@ public class TypedSensorValueList implements Serializable {
         return serialVersionUID;
     }
 
+    public String getScript() {
+        return script;
+    }
+
+    public void setScript(String script) {
+        this.script = script;
+    }
+
+    public String getVirtualSensorName() {
+        return virtualSensorName;
+    }
+
+    public void setVirtualSensorName(String virtualSensorName) {
+        this.virtualSensorName = virtualSensorName;
+    }
+
     @Override
     public String toString() {
-        String res = "";
-        for (TypedSensorValue t : sensorValues) {
-            res += "{\n" + t.toString() + "\n}\n";
-        }
         return "TypedSensorValueList{" +
-                res +
+                "script='" + script + '\'' +
+                ", virtualSensorName='" + virtualSensorName + '\'' +
+                ", sensorValues=" + sensorValues +
                 '}';
     }
 }

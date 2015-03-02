29be136582943fe5cf31bb2f79c643a8a2cf05c9
Romain Alexandre <alexandr@polytech.unice.fr>
2015-03-02 15:05:45 +0100

    Remove log

--- cep-engine/src/main/java/fr/unice/smart_campus/middleware/cep_engine/CEPEngine.java
+++ cep-engine/src/main/java/fr/unice/smart_campus/middleware/cep_engine/CEPEngine.java
@@ -170,7 +170,7 @@ public class CEPEngine {
     }
 
     public void sendEvent(CEPEvent event) {
-        System.out.println("Sending event to CEP : " + event.toString());
+        //System.out.println("Sending event to CEP : " + event.toString());
         cepRT.sendEvent(event);
     }
 

0fa437aeb923f34a6fbfa8364fd8c093a6d57812
Romain Alexandre <alexandr@polytech.unice.fr>
2015-03-04 23:43:06 +0100

    tired

--- virtual-sensor-processing/src/main/java/fr/unice/smart_campus/middleware/virtual_sensor_computing/Main.java
+++ virtual-sensor-processing/src/main/java/fr/unice/smart_campus/middleware/virtual_sensor_computing/Main.java
@@ -1,13 +1,15 @@
 package fr.unice.smart_campus.middleware.virtual_sensor_computing;
 
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 import com.typesafe.config.ConfigFactory;
+import fr.unice.smart_campus.middleware.akka.actor.ScriptEvaluatorActor;
 
 public class Main {
     public static void main(String[] args) {
         // creation of the actor system (Actor factory)
         ActorSystem actorSystem = ActorSystem.create("ActorSystemFactory", ConfigFactory.load());
-        //actorSystem.actorOf(Props.create(ScriptEvaluatorActor.class), "ScriptEvaluatorActor");
+        actorSystem.actorOf(Props.create(ScriptEvaluatorActor.class), "ScriptEvaluatorActor");
 
         //TODO virer nemo
 //        ActorRef nemo = actorSystem.actorOf(Props.create(ScriptEvaluatorActor.class), "PouitPouitActor");
